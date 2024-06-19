ce420b4cdc7c73a1b98462915a1a95cf44bf3f20
dralee <zhzl_s@126.com>
2024-06-19 14:15:48 +0800

    feat: check

--- src/Dralee.Generic.Xml/build.sh
+++ src/Dralee.Generic.Xml/build.sh
@@ -2,6 +2,6 @@ key=$1 # set the nuget key
 ver=$2 # set the version 1.0.0
 dotnet build -p:Version=$ver -c:Release
 cd bin/Release
-dotnet nuget push Dralee.Generic.Xml.$ver.nupkg -k $key -s https://api.nuget.org/v3/index.json --skip-duplicate
+dotnet nuget push Dralee.Generic.Xml.$ver.nupkg -k $key -s https://api.nuget.org/v3/index.json
 cd ../..
 echo finish
